12f53ab5dc6bb8a2cd4899eb7b90a3b13c27de8c
May Elisa Biusing <crowlord@Mays-MacBook-Pro.local>
2025-11-18 00:11:57 +0800

    edited ft_strtrim and added desc in ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -3,12 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbiusing <mbiusing@student.42kl.edu.m      +#+  +:+       +#+        */
+/*   By: mbiusing <mbiusing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 17:08:26 by mbiusing          #+#    #+#             */
-/*   Updated: 2025/10/26 23:46:02 by mbiusing         ###   ########.fr       */
+/*   Updated: 2025/11/17 23:47:42 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// DESC :	The strchr() function returns a pointer to the first occurrence of
+//			the character c in the string s.
+// RETURN :	The strchr() and strrchr() functions return a pointer to the
+//  		matched character or NULL if the character is not found.  The
+//			terminating null byte is considered part of the string, so that if
+//			c is specified as '\0', these functions return a pointer to the
+//			terminator.
 
 #include "libft.h"
 
